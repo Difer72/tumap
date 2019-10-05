d79eaeb01a9f905b1cd402400ed57c4ebc7faa8d
Diego Duarte <diego_72dalmata@hotmail.com>
2019-10-05 07:06:27 -0500

    proyectotumap

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'ubicacion', loadChildren: './dbo/ubicacion/ubicacion.module#UbicacionPageModule' },
+  { path: 'inscripcion', loadChildren: './dbo/inscripcion/inscripcion.module#InscripcionPageModule' },
+  { path: 'formulario', loadChildren: './dbo/formulario/formulario.module#FormularioPageModule' },
+  { path: 'exportar', loadChildren: './dbo/exportar/exportar.module#ExportarPageModule' }
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
